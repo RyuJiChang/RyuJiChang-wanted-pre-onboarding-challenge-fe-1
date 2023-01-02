19de70cb63aed1ac740199f0d504a82a36c6ba51
RyuJiChang <rgc0582@gmail.com>
2023-01-02 12:22:29 +0900

    App.tsx router 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+// pages import 해주기
 function App() {
-  return <div>정리 완료</div>;
+  return (
+    <Routes>
+      {/* 경로 수정 필요(PATH이용하기) */}
+      <Route path="/auth" element={<div>signup/signin 넣을 공간</div>} />
+      <Route path="/" element={<div>todo 내용 넣을 공간</div>} />
+    </Routes>
+  );
 }
 
 export default App;
